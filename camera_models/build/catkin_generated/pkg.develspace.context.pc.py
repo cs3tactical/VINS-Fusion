a1cc015e90543ff62ff5d4c055314bc006433201
Gil Meiri <barcesat@gmail.com>
2023-11-26 18:08:50 +0200

    converted cameramodels to openCV4

---
+++ camera_models/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/peterpan/camera_models_cv4/include".split(';') if "/home/peterpan/camera_models_cv4/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lcamera_models".split(';') if "-lcamera_models" != "" else []
+PROJECT_NAME = "camera_models"
+PROJECT_SPACE_DIR = "/home/peterpan/camera_models_cv4/build/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ camera_models/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/peterpan/camera_models_cv4/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/peterpan/.local/bin:/usr/local/cuda-11.4/bin:/home/peterpan/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/peterpan/new_nano_ws/src/resources/system:/home/peterpan/new_nano_ws/src/resources/system/peter'
+export PWD='/home/peterpan/camera_models_cv4/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/peterpan/camera_models_cv4/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/peterpan/camera_models_cv4:$ROS_PACKAGE_PATH"
